9341f839db0c88584974452c68a638d60dd2349f
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-05-15 18:56:24 +0800

    fix mobile responsive layout and styling

--- src/components/FooterComponents/Footer.jsx
+++ src/components/FooterComponents/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full  mx-auto bg-[#161616]  text-black pt-10">
+    <footer className="w-full  mx-auto bg-[#161616]  text-black pt-5">
       <div className="mx-auto w-full lg:max-w-[1000px] xl:max-w-[1100px] 2xl:max-w-[1280px]">
-        <div className="footer-links my-6 flex flex-col items-center space-y-6 pb-2 text-center text-white lg:flex-row lg:items-start lg:space-y-0 lg:space-x-12 lg:pb-0 lg:text-left">
+        <div className="footer-links my-6 flex items-start px-5 lg:px-0 space-x-6 pb-2 text-white">
           <div className="customer-service">
             <h1 className="mb-1 font-bold">Customer Service</h1>
             <ul className="space-y-2 font-light">
@@ -26,7 +26,7 @@ const Footer = () => {
         </div>
         <hr className="my-2" />
         <div className="footer-text-info flex flex-col-reverse items-center justify-between p-4 text-center font-normal text-white">
-          <p>© 2022 Shappy. All Rights Reserved.</p>
+          <p>© 2023 Shappy. All Rights Reserved.</p>
           <div className="payment-info flex flex-col items-center pb-4">
             <p>We accept the following payment methods.</p>
             <img
