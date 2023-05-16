82dfd3a83d23c6a96464f70bdaec32c6389ca5c7
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-05-16 15:03:34 +0800

    adjust product card component colors

--- src/components/NavbarComponents/NavbarLinks.jsx
+++ src/components/NavbarComponents/NavbarLinks.jsx
@@ -25,12 +25,15 @@ const NavbarLinks = ({ isMenuOpen, setMenuIsOpen }) => {
       <div className="items-center space-x-1 flex">
         {user ? (
           <ul className="flex flex-col md:flex-row lg:space-x-4">
-            <li className="flex space-x-2 items-center">
+            <li className="flex space-x-2 items-center cursor-pointer">
               <Heart color="#808080" weight="fill" size={30} />
               <p className="md:hidden">Wishlist</p>
             </li>
 
-            <li onClick={() => setMenuIsOpen(false)}>
+            <li
+              onClick={() => setMenuIsOpen(false)}
+              className="flex items-center"
+            >
               <Link to={"/cart"} className="relative flex items-center">
                 {onCart > 0 && (
                   <div className="absolute -top-1 left-5 bg-green-500/90 rounded-full h-3 w-3 md:h-5 md:w-5 text-white flex items-center justify-center p-2">
@@ -42,10 +45,11 @@ const NavbarLinks = ({ isMenuOpen, setMenuIsOpen }) => {
               </Link>
             </li>
 
-            <li className="flex space-x-2 items-center">
+            <li className="flex space-x-2 md:space-x-1 items-center md:border md:border-neutral-300 md:p-1 rounded-full cursor-pointer">
               <UserCircle size={30} weight="light" />
               <p>{user.email}</p>
             </li>
+
             <li
               className="flex space-x-2 items-center cursor-pointer hover:text-neutral-800 ease-in duration-150 mt-1"
               onClick={handleLogout}
