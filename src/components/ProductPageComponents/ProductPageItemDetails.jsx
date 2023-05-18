7b920f400c83d2769111a6c48f72838ad7098915
mango_gills <64761137+mango-gills@users.noreply.github.com>
2023-05-18 15:37:19 +0800

    fix error in imports

--- src/components/ProductPageComponents/ProductPageItemDetails.jsx
+++ src/components/ProductPageComponents/ProductPageItemDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import AddToCart from "./AddToCart";
-import RecommendedProducts from "../RecommendedProductsComponent/RecommendedProducts";
+import RecommendedProducts from "../ProductsCarouselComponent/RecommendedProducts";
 
 const ProductPageItemDetails = () => {
   const [item, setItem] = useState([]);
